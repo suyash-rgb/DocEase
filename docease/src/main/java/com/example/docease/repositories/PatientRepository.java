8d882d09dd55f9e2e7fc5475ce191e87648177c3
Suyash Baoney <suyashbaoney58@gmail.com>
2025-05-15 12:32:32 +0530

    corrected property reference in PatientRepository

--- docease/src/main/java/com/example/docease/repositories/PatientRepository.java
+++ docease/src/main/java/com/example/docease/repositories/PatientRepository.java
@@ -2,6 +2,7 @@ package com.example.docease.repositories;
 
 import com.example.docease.entities.Patient;
 import com.example.docease.entities.Role;
+import com.example.docease.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository <Patient, Integer>{
-    Optional<Patient> findByUserId(Integer userId);
+    Optional<Patient> findByUser(User user);
 }
