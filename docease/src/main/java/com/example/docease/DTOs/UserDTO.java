e0eafcbb89c6067d72c9fe5103c86b85b2e6f84a
Suyash <144018815+suyash-rgb@users.noreply.github.com>
2025-09-15 22:51:04 +0530

    Delete docease directory

--- docease/src/main/java/com/example/docease/DTOs/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.docease.DTOs;
-
-public class UserDTO {
-    private String username;
-    private String password;
-    private String email;
-    private String phone;
-    private String role;  // "DOCTOR" or "PATIENT"
-}
--- docease/src/main/java/com/example/docease/controllers/PatientController.java
+++
@@ -1,4 +0,0 @@
-package com.example.docease.controllers;
-
-public class PatientController {
-}
