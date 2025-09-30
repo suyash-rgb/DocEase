7fb7fe5fc7d17cd18dd49eadbd91db5dd83de640
Suyash Baoney <suyashbaoney58@gmail.com>
2025-09-30 22:19:05 +0530

    basic microservice setup

---
+++ docease-backend/aiservice/src/main/java/com/docease/aiservice/AiserviceApplication.java
@@ -0,0 +1,14 @@
+package com.docease.aiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class AiserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiserviceApplication.class, args);
+	}
+
+}
