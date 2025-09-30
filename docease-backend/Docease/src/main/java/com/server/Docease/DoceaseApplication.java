035fbd9b0e247fdfd3200b516cba0986708089ab
Suyash Baoney <suyashbaoney58@gmail.com>
2025-09-30 17:05:26 +0530

    Adding spring cloud server service

---
+++ docease-backend/Docease/src/main/java/com/server/Docease/DoceaseApplication.java
@@ -0,0 +1,15 @@
+package com.server.Docease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DoceaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoceaseApplication.class, args);
+	}
+
+}
---
+++ docease-backend/Docease/src/test/java/com/server/Docease/DoceaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.Docease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoceaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
